7b51584f008bf99f5665605102c72b5dfe5ce435
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2015-03-13 23:09:13 +0100

    New snippets

--- _vim/bundle/update.py
+++ _vim/bundle/update.py
@@ -2,23 +2,40 @@
 
 import os
 
-f = open('modules').readlines()
-for line in f:
-    l = line.split()[1]
-    if not os.path.exists(l):
-        if line.startswith("#"):
-            continue
-        print "installing %s...." % line
-        if "bitbucket" in line:
-            os.system('hg clone %s' % line)
-        else:
-            os.system('git clone %s' % line)
-    else:
-        os.chdir(l)
-        print "updating %s...." % line
-        if "bitbucket" in line:
-            os.system('hg pull')
-            os.system('hg up')
-        else:
-            os.system("git pull")
-        os.chdir("..")
+
+# Make wrapper for printing
+
+def printing(text, line):
+    print(80*"=")
+    print("%s %s...." % (text, line))
+
+
+def update():
+    with open('modules') as f:
+        for line in f:
+            l = line.split()[1]
+            if not os.path.exists(l):
+                if line.startswith("#"):
+                    continue
+
+                printing("installing", line)
+
+                if "bitbucket" in line:
+                    os.system('hg clone %s' % line)
+                else:
+                    os.system('git clone %s' % line)
+                print 80*"="
+            else:
+                os.chdir(l)
+                printing("updating", line)
+                if "bitbucket" in line:
+                    os.system('hg pull')
+                    os.system('hg up')
+                else:
+                    os.system("git pull")
+                print 80*"="
+                os.chdir("..")
+
+
+if __name__ == '__main__':
+    update()
