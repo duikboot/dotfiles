9231b0f7e0cec370427c8db8b762b3c3d10b0739
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2021-01-06 14:32:58 +0100

    added lua language server again)

--- config/nvim/lua/duikboot/lsp.lua
+++ config/nvim/lua/duikboot/lsp.lua
@@ -48,11 +48,44 @@ local on_attach_vim_plus_keymaps = function(client)
     mapper('n', '<c-]>',      '<cmd> lua vim.lsp.buf.definition()<CR>')
 end
 
+
+local system_name
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system for sumneko")
+end
+-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
+}
+
 lspconfig.jedi_language_server.setup( {on_attach=on_attach_vim_plus_keymaps })
--- lspconfig.lua_language_server.setup(
---     { on_attach=on_attach_vim_plus_keymaps },
---     {settings = {
---         Lua = {
---             diagnostics = {
---                 globals = { 'vim'}}}}})
 lspconfig.vimls.setup( { on_attach=on_attach_vim_plus_keymaps })
