4ca8d47e4b6e4b1b1174ddd9714985fbbb75e2e2
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2021-05-28 10:50:37 +0200

    buftabline

---
+++ config/nvim/lua/duikboot/buftabline.lua
@@ -0,0 +1,9 @@
+require("buftabline").setup {
+    kill_maps = true,
+    custom_command = "vertical sb",
+    custom_map_prefix = "v",
+    auto_hide = true,
+    icons = true,
+    icon_colors = "normal",
+        next_indicator = ">",
+}
