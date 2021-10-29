12685fdc091798784cd69a3219f7eada0579bfa7
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2021-10-29 22:09:19 +0200

    cmp for completion pylsp

--- config/nvim/lua/duikboot/lsp.lua
+++ config/nvim/lua/duikboot/lsp.lua
@@ -14,39 +14,39 @@ local lspconfig = require'lspconfig'
 vim.b.lsp_diagnostics_enabled = true
 
 local function disable()
-  vim.b.lsp_diagnostics_enabled = false
+    vim.b.lsp_diagnostics_enabled = false
 
-  -- Clear existing diagnostics
-  local clients = vim.lsp.get_active_clients()
-  for _, client in pairs(clients) do
-    vim.lsp.diagnostic.clear(vim.fn.bufnr(), client.id, nil, nil)
-  end
+    -- Clear existing diagnostics
+    local clients = vim.lsp.get_active_clients()
+    for _, client in pairs(clients) do
+        vim.lsp.diagnostic.clear(vim.fn.bufnr(), client.id, nil, nil)
+    end
 end
 
 local function enable()
-  vim.b.lsp_diagnostics_enabled = true
+    vim.b.lsp_diagnostics_enabled = true
 
-  -- Cheat and reload the file to trigger a `publishDiagnostics` event.
-  -- TODO: Find a way to actually reload the thing :(
-  vim.cmd('edit')
+    -- Cheat and reload the file to trigger a `publishDiagnostics` event.
+    -- TODO: Find a way to actually reload the thing :(
+    vim.cmd('edit')
 end
 
 function Toggle_diagnostics()
-  if vim.b.lsp_diagnostics_enabled then
-    disable()
-  else
-    enable()
-  end
+    if vim.b.lsp_diagnostics_enabled then
+        disable()
+    else
+        enable()
+    end
 end
 
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-    signs = true,
-    update_in_insert = false,
-    underline = false
-  }
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+        underline = false
+    }
 )
 
 local on_init = function()
@@ -55,14 +55,14 @@ end
 
 local default_handler = vim.lsp.handlers["textDocument/definition"]
 vim.lsp.handlers["textDocument/definition"] = function(_, method, result)
-  if result ~= nil then
-    default_handler(nil, method, result)
-  else
-    -- there's a literal "ctrl-]" after `normal!`, might be able to craft
-    -- something with `execute "normal! \<C-]>"` instead for something more
-    -- readable.
-    vim.cmd("normal! ")
-  end
+    if result ~= nil then
+        default_handler(nil, method, result)
+    else
+        -- there's a literal "ctrl-]" after `normal!`, might be able to craft
+        -- something with `execute "normal! \<C-]>"` instead for something more
+        -- readable.
+        vim.cmd("normal! ")
+    end
 end
 
 
@@ -71,13 +71,13 @@ local on_attach_vim_plus_keymaps = function(client)
         'n',
         '<leader>dn',
         '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>'
-        )
+    )
 
     Mapper(
         'n',
         '<leader>dp',
         '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>'
-        )
+    )
     Mapper("n", "<leader>ta", '<cmd>lua Toggle_diagnostics()<cr>')
     Mapper('n', '<leader>dl', '<cmd> lua vim.lsp.diagnostic.set_loclist()<CR>')
     Mapper('n', '<leader>td', '<cmd> lua vim.lsp.buf.type_definition()<CR>')
@@ -101,53 +101,68 @@ end
 local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
 local sumneko_binary = "/bin/lua-language-server"
 require'lspconfig'.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  on_attach=on_attach_vim_plus_keymaps ,
-  on_init=on_init,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    on_attach=on_attach_vim_plus_keymaps ,
+    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    on_init=on_init,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';'),
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                },
+            },
         },
-      },
     },
-  },
 }
 
-local local_lsps = {
-    'pylsp',
-    -- 'pyright',
-    -- 'jedi_language_server',
-    'vimls',
-    'bashls'
+lspconfig['pylsp'].setup{
+    -- on_init=on_init,
+    on_attach=on_attach_vim_plus_keymaps,
+    -- capabilities=lsp_status.capabilities
+    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    settings = {
+        pylsp = {
+            plugins = {
+                jedi_completion = {
+                    include_params = true,
+                },
+            },
+        },
+    },
 }
 
-for _, lsp in ipairs(local_lsps) do
-    lspconfig[lsp].setup{
-        -- on_init=on_init,
-        on_attach=on_attach_vim_plus_keymaps,
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-        -- capabilities=lsp_status.capabilities
-    }
-end
+lspconfig['vimls'].setup{
+    -- on_init=on_init,
+    on_attach=on_attach_vim_plus_keymaps,
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    -- capabilities=lsp_status.capabilities
+}
+
+lspconfig['bashls'].setup{
+    -- on_init=on_init,
+    on_attach=on_attach_vim_plus_keymaps,
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    -- capabilities=lsp_status.capabilities
+}
+
 
 require("trouble").setup {
 -- your configuration comes here
 -- or leave it empty to use the default settings
--- refer to the configuration section below
+    -- refer to the configuration section below
 }
 
 -- lspconfig.pylsp.setup({
