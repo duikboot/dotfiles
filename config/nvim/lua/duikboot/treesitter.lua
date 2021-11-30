671eca73b1cd99f7b496db3c30b74c6b4bada25e
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2021-11-30 14:55:05 +0100

    Don't highlight current scope.

--- config/nvim/lua/duikboot/treesitter.lua
+++ config/nvim/lua/duikboot/treesitter.lua
@@ -65,7 +65,7 @@ require'nvim-treesitter.configs'.setup {
     },
     refactor = {
         highlight_definitions = {enable = true},
-        highlight_current_scope = {enable = true},
+        highlight_current_scope = {enable = false},
         navigation = {
             enable = true,
             keymaps = {
