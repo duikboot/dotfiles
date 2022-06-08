54daf28e434e1d872b0c11a01aa6f736c1335891
Arjen Dijksta <dijkstra.arjen@gmail.com>
2022-06-08 18:31:17 +0200

    dap

---
+++ config/nvim/lua/duikboot/dap.lua
@@ -0,0 +1,15 @@
+require('dap-python').setup('~/.dotfiles/.venv/bin/python')
+require("nvim-dap-virtual-text").setup()
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
+vim.keymap.set("n", "<F5>", ":lua require'dap'.toggle_breakpoint()")
