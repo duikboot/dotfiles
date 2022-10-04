daca691b86c89b8dd8d1a70c5ac17b9500ade5f7
Arjen Dijksta <dijkstra.arjen@gmail.com>
2022-10-04 21:27:48 +0200

    Make

--- config/nvim/lua/duikboot/treesitter.lua
+++ config/nvim/lua/duikboot/treesitter.lua
@@ -40,20 +40,21 @@ require'nvim-treesitter.configs'.setup {
                 ["<leader>pc"] = "@class.outer"
             }
         },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = {"@function.outer", "@class.outer"},
-        },
-        goto_next_end = {
-          ["]M"] = {"@function.outer", "@class.outer"},
-        },
-        goto_previous_start = {
-          ["[m"] = {"@function.outer", "@class.outer"},
-        },
-        goto_previous_end = {
-          ["[M"] = {"@function.outer", "@class.outer"},
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = {"@function.outer", "@class.outer"},
+            },
+            goto_next_end = {
+                ["]M"] = {"@function.outer", "@class.outer"},
+            },
+            goto_previous_start = {
+                ["[m"] = {"@function.outer", "@class.outer"},
+            },
+            goto_previous_end = {
+                ["[M"] = {"@function.outer", "@class.outer"},
+            }
         },
         select = {
             enable = true,
