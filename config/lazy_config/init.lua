cfd024b5f02fa2a542deeb9169db03c1fac72f03
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2023-08-06 18:28:54 +0200

    git.

--- config/lazy_config/init.lua
+++ config/lazy_config/init.lua
@@ -1,6 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
+        "git",
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
