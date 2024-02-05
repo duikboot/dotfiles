bf702c214d18b5f3a4a792f0194e3810c7158cfa
Arjen Dijkstra <arjen.dijkstra@coolblue.nl>
2024-02-05 16:16:23 +0100

    Able to open harpoon thingies in splits

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -19,3 +19,15 @@ for i=1,9 do
             harpoon:list():select(i)
         end, {desc="Got to harpoon: "..i})
 end
+
+harpoon:extend({
+    UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function()
+            harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set("n", "<C-x>", function()
+            harpoon.ui:select_menu_item({ split = true })
+        end, { buffer = cx.bufnr })
+    end,
+})
