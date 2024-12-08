338c2d819fe6b714e7ef66a7f7a05620046b6eec
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2024-12-08 10:09:46 +0100

    more

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -314,6 +314,16 @@ require("lazy").setup({
         end
     },
     {"https://github.com/fcying/telescope-ctags-outline.nvim", },
+  {
+    'https://github.com/MagicDuck/grug-far.nvim',
+    config = function()
+      require('grug-far').setup({
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      });
+    end
+  },
 
     -- dap
     "https://github.com/mfussenegger/nvim-dap",
