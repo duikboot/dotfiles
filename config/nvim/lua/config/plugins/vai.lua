a3b01c5dae906af439b6a7b43af66d1b6a25b638
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2026-02-13 14:23:25 +0100

    Vai

--- config/nvim/lua/config/plugins/vai.lua
+++ config/nvim/lua/config/plugins/vai.lua
@@ -1,11 +1,11 @@
 -- lazy.nvim
 return {
-  'https://github.com/johnpmitsch/vai.nvim',
+    'https://github.com/johnpmitsch/vai.nvim',
     config = function()
-    require('vai').setup(
+        require('vai').setup(
             {
-            trigger='<leader><cr>'
-}
-    )
-  end,
+                trigger = '<leader><cr>'
+            }
+        )
+    end,
 }
